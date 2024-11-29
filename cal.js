c84a46e2949e807ef4c80d519e3c37ea53671ed7
venkat08062002 <venkat08062002@gmail.com>
2024-11-29 15:24:52 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,20 @@
+const display = document.getElementById('display');
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+function delet(){
+    display.value=display.value.slice(0,-1);
+}
